72e61019f36bff2509547b2a6612694c99059705
Chengzhu Yu <cyu0913@gmail.com>
2016-04-09 21:25:17 -0500

    started scripts.CY

---
+++ scripts/run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+log_start(){
+  echo "#####################################################################"
+  echo "Spawning *** $1 *** on" `date` `hostname`
+  echo ---------------------------------------------------------------------
+}
+
+log_end(){
+  echo ---------------------------------------------------------------------
+  echo "Done *** $1 *** on" `date` `hostname`
+  echo "#####################################################################"
+}
+
+. cmd.sh
+. path.sh
+
+set -e # exit on error
+
+run_mfcc(){
+    mfccdir=mfcc
+    for x in toy; do
+      steps/make_mfcc.sh --cmd "$train_cmd" --nj 1 data/$x exp/make_mfcc/$x $mfccdir || exit 1;
+      steps/compute_cmvn_stats.sh data/$x exp/make_mfcc/$x $mfccdir || exit 1;
+      #utils/fix_data_dir.sh data/$x
+    done
+}
+run_mfcc
+
