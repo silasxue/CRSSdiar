1ba1023c25dd3e0ed0b74c69fc0170e6b5767ae8
idnavid <navid.shokouhi@utdallas.edu>
2016-07-22 16:02:59 -0500

    recovered lost test.sh, plus a new function run_diarization

--- scripts/test.sh
+++ scripts/test.sh
@@ -17,66 +17,68 @@ log_end(){
 
 set -e # exit on error
 
+data="toy_3"
 run_mfcc(){
-    log_start "Extract MFCC features"	
+    log_start "Extract MFCC features"
 
     mfccdir=mfcc
-    for x in toy_3; do
+    datadir=$1
+    for x in $datadir; do
       steps/make_mfcc.sh --cmd "$train_cmd" --nj 1 data/$x exp/make_mfcc/$x $mfccdir || exit 1;
       steps/compute_cmvn_stats.sh data/$x exp/make_mfcc/$x $mfccdir || exit 1;
     done
 
-    log_end "Extract MFCC features"	
+    log_end "Extract MFCC features"
 }
-run_mfcc
+#run_mfcc $data
 
 run_vad(){
-    log_start "Doing VAD"	
-
-    for x in toy_3; do
-   	 vaddir=exp/vad/$x
-   	 diar/compute_vad_decision.sh --nj 1 data/$x $vaddir/log $vaddir 	
-    done	
-    log_end "Finish VAD"	
+    log_start "Doing VAD"
+    datadir=$1
+    for x in $datadir; do
+     vaddir=exp/vad/$x
+     diar/compute_vad_decision.sh --nj 1 data/$x $vaddir/log $vaddir
+    done
+    log_end "Finish VAD"
 }
-#run_vad
+#run_vad $data
 
 make_ref(){
-    log_start "Generate Reference Segments/Labels/RTTM files"	
+    log_start "Generate Reference Segments/Labels/RTTM files"
 
     #ami_annotated_segment=/home/chengzhu/work/SpeechCorpus/ami_dir/segments
     ami_annotated_segment=/home/nxs113020/Downloads/ami_dir/segments
 
-    for x in toy_3; do
-    	local/make_ami_ref.sh data/$x $ami_annotated_segment exp/ref/$x 
-    done	
+    datadir=$1
+    for x in $datadir; do
+        local/make_ami_ref.sh data/$x $ami_annotated_segment exp/ref/$x
+    done
 
-    log_end "Generate Reference Segments/Labels/RTTM files"	
+    log_end "Generate Reference Segments/Labels/RTTM files"
 }
-make_ref
+#make_ref $data 
 
 test_ivectors(){
-    for x in toy_3; do
+
+    datadir=$1
+    for x in $datadir; do
         diar/test_ivector_score.sh --nj 1 exp/extractor_1024 data/$x exp/ref/$x/labels exp/temp/test_ivectors
     done
 }
-#test_ivectors
+#test_ivectors $data
 
-#IvectorExtract(){
-#    sid/extract_segment_ivector.sh --nj 1 exp/extractor_1024 data/toy local/label.ark exp/segment_ivectors
-#}
-#IvectorExtract
 
 run_changedetection() {
-    log_start "Run Change Detection Using BIC"	
+    log_start "Run Change Detection Using BIC"
 
-    for x in toy_3; do
-	    local/change_detect_bic.sh data/$x exp/ref/$x exp/change_detect/$x
+    datadir=$1
+    for x in $datadir; do
+        local/change_detect_bic.sh data/$x exp/ref/$x exp/change_detect/$x
     done
-	
-    log_end "Run Change Detection Using BIC"	
+
+    log_end "Run Change Detection Using BIC"
 }
-run_changedetection
+#run_changedetection $data
 
 train_extractor(){
     ubmdim=1024
@@ -93,40 +95,58 @@ train_extractor(){
       exp/extractor_ES20 || exit 1;
 }
 #train_extractor
-
 run_glpkIlpTemplate(){
-    log_start "Generate GLPK Template of ILP problem "	
+    log_start "Generate GLPK Template of ILP problem "
 
-    for x in toy_3; do
-   	    #diar/generate_ILP_template.sh --nj 1 --seg_min 0 --delta 30 exp/extractor_1024.swbd data/$x exp/ref/$x/segments exp/glpk_template/$x
-    	#diar/generate_ILP_template.sh --nj 1 --seg_min 50 --delta 30 exp/extractor_1024.swbd data/$x exp/change_detect/$x/segments exp/glpk_template/$x
-   	    diar/generate_ILP_template.sh --nj 1 --seg_min 0 --delta 30 exp/extractor_1024.swbd data/$x exp/ref/$x/vad_label/segments exp/glpk_template/$x
+    datadir=$1
+    for x in $datadir; do
+        diar/generate_ILP_template.sh --nj 1 --seg_min 50 --delta 30 exp/extractor_1024 data/$x exp/change_detect/$x/segments
     done
 
-    log_end "Generate GLPK Template of ILP problem "	
+    log_end "Generate GLPK Template of ILP problem "
 }
-#run_glpkIlpTemplate
+#run_glpkIlpTemplate $data
 
 run_glpk_Ilp(){
-    log_start "Run ILP Clustering"	
+    log_start "Run ILP Clustering"
 
-    for x in toy_3; do
-	    #diar/ILP_clustering.sh --seg_min 0 exp/glpk_template/$x exp/ref/$x/segments exp/glpk_ilp/$x
-	    #diar/ILP_clustering.sh --seg_min 50 exp/glpk_template/$x exp/change_detect/$x/segments exp/glpk_ilp/$x
-	    diar/ILP_clustering.sh --seg_min 0 exp/glpk_template/$x exp/ref/$x/vad_label/segments exp/glpk_ilp/$x
+    datadir=$1
+    for x in $datadir; do
+        diar/ILP_clustering.sh --seg_min 50 exp/glpk_template/$x exp/change_detect/$x/segments exp/glpk_ilp/$x
     done
 
-    log_end "Run ILP Clustering"	
+    log_end "Run ILP Clustering"
 }
-#run_glpk_Ilp
+#run_glpk_Ilp $data
 
 run_DER(){
-    log_start "Compute Diarization Error Rate (DER)"	
- 
-    for x in toy_3; do
-       diar/compute_DER.sh exp/ref/$x/rttms exp/glpk_ilp/$x/rttms exp/result_DER/$x	
-    done	
+    log_start "Compute Diarization Error Rate (DER)"
+    datadir=$1
+    for x in $datadir; do
+       diar/compute_DER.sh exp/ref/$x/rttms exp/glpk_ilp/$x/rttms exp/result_DER/$x
+    done
 
-    log_end "Compute Diarization Error Rate (DER)"	
+    log_end "Compute Diarization Error Rate (DER)"
 }
-#run_DER
+#run_DER $data
+
+
+run_diarization(){
+    # Perform diarization on each file (separately):
+    datadir=$1
+    # split data directory into individual files. 
+    nfiles=`local/split_data_dir.sh data/$datadir | cut -d ' ' -f 1`
+    fileidx=1
+    while [ $fileidx -le $nfiles ]; do
+        run_changedetection $datadir_file_$fileidx
+        fileidx=$[$fileidx+1]
+    done
+}
+run_diarization $data
+
+
+
+
+
+
+
